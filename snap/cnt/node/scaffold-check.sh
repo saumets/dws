b5335ecd3f821ed624e59d3a2b89af34e4b48cf1
Paul Saumets <psaumets@gmail.com>
2015-12-07 14:39:58 -0500

    fix to allow for file watching.

--- snap/cnt/node/scaffold-check.sh
+++ snap/cnt/node/scaffold-check.sh
@@ -10,3 +10,9 @@ if [ -d "$WORK_DIR" ]; then
     cd $WORK_DIR && sails new .
   fi 
 fi
+
+# required for file watching in container
+# http://stackoverflow.com/questions/16748737/grunt-watch-error-waiting-fatal-error-watch-enospc
+if [ ! grep -Fxq "/etc/sysctl.conf" "fs.inotify.max_user_watches=524288" ]; then
+  echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+fi 
